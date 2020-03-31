8afe6a69be69017a531917d3aeb395d35cc55273
unknown <nityacohen10@gmail.com>
2020-03-31 13:21:54 -0400

    equityEMA() function works

--- Python/technicalAnalysis.py
+++ Python/technicalAnalysis.py
@@ -7,17 +7,18 @@ class technicalAnalysis:
     def __init__(self, api_key):
         self.ALPHA_VANTAGE_API_KEY = api_key
 
-    def equityEMA(self, symbol, interval, timePeriod): # Only set up for EMA
+    def equityEMA(self, symbol: str, interval: str, timePeriod: str):  # Only set up for EMA
         resp = requests.get("https://www.alphavantage.co/query?function=EMA&symbol="
                             + symbol + "&interval=" + interval + "&time_period=" + timePeriod +
                             "&series_type=open&apikey=" + self.ALPHA_VANTAGE_API_KEY)
 
         if resp.status_code != 200:
             raise Exception("Error in request to Alpha Vantage.\n Response was: {}".format(resp))
-        data = resp.content
-        currentEMA = data["Technical Analysis: EMA"]
-    # def getPrice(self, symbol):
-    #     resp = requests
+        data = resp.json()
+        dictOfEMA = data["Technical Analysis: EMA"]
+        currentEMA = dictOfEMA[next(iter(dictOfEMA))]["EMA"]  # get first key from json/EMA dict
+        return currentEMA
+
 
 if __name__ == '__main__':
     ta = technicalAnalysis("MA6YR6D5TVXK1W67")
@@ -26,6 +27,6 @@ if __name__ == '__main__':
     #                                   "https://finance.yahoo.com/screener/predefined/growth_technology_stocks")
     # ticks = scraper.generateTickers()
     ticks = "SHOP"
-    ta.equityEMA(symbol=ticks, interval="daily", timePeriod=20) # Maybe the low EMA?
-    ta.equityEMA(symbol=ticks, interval="daily", timePeriod=50)
+    print(ta.equityEMA(symbol=ticks, interval="daily", timePeriod="20"))
+    print(ta.equityEMA(symbol=ticks, interval="daily", timePeriod="50"))
 
