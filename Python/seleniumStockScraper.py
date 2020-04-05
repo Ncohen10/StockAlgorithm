e71aea1f5cc14a8cb8557c3d157e3bb8e412b381
unknown <nityacohen10@gmail.com>
2020-04-05 13:52:56 -0400

    Improved time complexity and added functions

--- Python/seleniumStockScraper.py
+++ Python/seleniumStockScraper.py
@@ -28,20 +28,19 @@ class seleniumStockScraper:
         changeButton = "th.Ta\(end\):nth-child(4) > span:nth-child(1)"  # CSS Selector path of "change" button on Yahoo
         stockListings = "tr.simpTblRow:nth-child(1) > td:nth-child(1) > a:nth-child(2)"
         self.driver.get(yahooScreenURL)
-        WebDriverWait(self.driver, timeout).until(   # wait for changeButton to load
-            lambda x: x.find_element_by_css_selector(changeButton))
-        # Need to hardcode wait times since clicking on button doesn't do anything even though the page is fully loaded
-        # Due to Yahoo website design...
-        time.sleep(5)
-        print("wait 1 complete")
-        self.driver.find_element_by_css_selector(changeButton).click()
-        WebDriverWait(self.driver, timeout).until(  # wait again after pressing once...
-            lambda x: x.find_element_by_css_selector(changeButton))
-        time.sleep(5)
-        print("wait 2 complete")
-        self.driver.find_element_by_css_selector(changeButton).click()
-        WebDriverWait(self.driver, timeout).until(
-            lambda x: x.find_element_by_css_selector(stockListings))  # Now wait for stock tickers to load
+        # WebDriverWait(self.driver, timeout).until(   # wait for changeButton to load
+        #     lambda x: x.find_element_by_css_selector(changeButton))
+        # # Need to hardcode wait times since clicking on button doesn't do anything even though the page is fully loaded
+        # # Due to Yahoo website design...
+        # time.sleep(10)
+        # self.driver.find_element_by_css_selector(changeButton).click()
+        # WebDriverWait(self.driver, timeout).until(  # wait again after pressing once...
+        #     lambda x: x.find_element_by_css_selector(changeButton))
+        # time.sleep(10)
+        # self.driver.find_element_by_css_selector(changeButton).click()
+        # WebDriverWait(self.driver, timeout).until(
+        #     lambda x: x.find_element_by_css_selector(stockListings))  # Now wait for stock tickers to load
+        time.sleep(20)
 
     # Gets stocks from Yahoo finance page
     def _selectTickers(self) -> list:
@@ -64,8 +63,6 @@ class seleniumStockScraper:
                 else:
                     attempts += 1
                     pass
-
-        print(tickers)
         return tickers
 
     def generateTickers(self) -> list:
@@ -79,5 +76,3 @@ if __name__ == '__main__':
                                       "../geckodriver.exe",
                                       "https://finance.yahoo.com/screener/predefined/growth_technology_stocks")
     print(webScraper.generateTickers())
-
-# ['STNE', 'SMAR', 'SHOP', 'ZS', 'PAGS', 'EGHT', 'PS', 'RNG', 'COUP', 'OKTA', 'YEXT', 'MLNX', 'PAYC', 'ALRM', 'QTWO', 'OLED', 'HMI', 'OPRA', 'OCFT']
--- Python/technicalAnalysis.py
+++ Python/technicalAnalysis.py
@@ -12,18 +12,11 @@ class technicalAnalysis:
         resp = requests.get("https://www.alphavantage.co/query?function=EMA&symbol="
                             + symbol + "&interval=" + interval + "&time_period=" + timePeriod +
                             "&series_type=open&apikey=" + self.ALPHA_VANTAGE_API_KEY)
-
         if resp.status_code != 200:
             raise Exception("Error in request to Alpha Vantage. Response was:\n {}".format(resp))
         data = resp.json()
         dictOfEMA = data.get("Technical Analysis: EMA")
-        if not dictOfEMA:
-            return []
-        EMAList = []
-        for EMA in dictOfEMA:
-            currentEMA = float(dictOfEMA[EMA]["EMA"])  # get first key from json/EMA dict, then get its EMA.
-            EMAList.append(currentEMA)
-        return EMAList
+        return dictOfEMA
 
     def getPrice(self, symbol: str) -> list:
         resp = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
@@ -32,30 +25,33 @@ class technicalAnalysis:
             raise Exception("Error in request to Alpha Vantage. Response was:\n {}".format(resp))
         data = resp.json()
         dictOfPrices = data.get("Time Series (Daily)")
-        if not dictOfPrices:
-            return []
-        priceList = []
-        for price in dictOfPrices:
-            currentPrice = float(dictOfPrices[price]["3. low"])
-            priceList.append(currentPrice)
-        return priceList
+        return dictOfPrices
 
     @staticmethod
-    def testCrossover(twentyEMA: list, fiftyEMA: list, prices: list):
+    def recentCrossover(twentyEMA: list, fiftyEMA: list, prices: list) -> bool:
         day = 0
-        crossovers = 0
-        numOfDays = len(twentyEMA)
-        dipFound = False
-        while day < numOfDays and twentyEMA[day] > fiftyEMA[day]:
-            if prices[day] < twentyEMA[day]:
-                dipFound = True
-            elif dipFound and prices[day] > twentyEMA[day]:
-                dipFound = False
-                crossovers += 1
-            if crossovers == 3:
-                print("Buy")
+        numDays = 50
+        while day <= numDays:
+            if twentyEMA[day] > fiftyEMA[day]:
+                if prices[day] > twentyEMA[day]:
+                    print("{} is price. Greater than EMA ({}) at day {}".format(prices[day], twentyEMA[day], day))
+                else:
+                    print("{} is price. LESS than EMA ({}) at day {}".format(prices[day], twentyEMA[day], day))
                 return True
-            day += 1
+        return False
+
+    @staticmethod
+    def isPotentialCrossover(twentyEMA: list, fiftyEMA: list, prices: list):
+        if not twentyEMA or not fiftyEMA:
+            print("One or more EMAs are empty")
+            return False
+        if twentyEMA[0] > fiftyEMA[0]:
+            return False
+        diff = abs(fiftyEMA[0] - twentyEMA[0])
+        tolerance = 0.05
+        if diff // max(fiftyEMA[0], twentyEMA[0]) <= tolerance:  # if difference is within 5%
+            print("within range")
+            return True
         return False
 
 
@@ -65,9 +61,10 @@ if __name__ == '__main__':
                                    "../geckodriver.exe",
                                    "https://finance.yahoo.com/screener/predefined/growth_technology_stocks")
     ticks = scraper.generateTickers()
+    print(ticks)
     count = 0
     for stock in ticks:
-        if count % 5 == 0 and count != 0: time.sleep(65)
+        if count % 5 == 0: time.sleep(65)
         tEMA = ta.getEMA(stock, "20")
         count += 1
         if count % 5 == 0: time.sleep(65)
@@ -76,8 +73,11 @@ if __name__ == '__main__':
         if count % 5 == 0: time.sleep(65)
         priceHistory = ta.getPrice(stock)
         count += 1
-        if tEMA > fEMA:
-            print("Testing: {}".format(stock))
-            isCrossover = ta.testCrossover(tEMA, fEMA, priceHistory)
+        print("testing: {}".format(stock))
+        if tEMA: print("tEMA: {}".format(tEMA[0]))
+        if fEMA: print("fEMA: {}".format(fEMA[0]))
+        if priceHistory: print("price: {}".format(priceHistory[0]))
+        if tEMA and fEMA and tEMA[0] > fEMA[0]:
+            isCrossover = ta.recentCrossover(tEMA, fEMA, priceHistory)
             if isCrossover:
                 print("FOUND CROSSOVER!")
